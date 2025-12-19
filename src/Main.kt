96ba6d5b138c3d8bcf479e87cda4323f55ce46a5
sashish <sashish@rediff-inc.com>
2025-12-19 17:08:51 +0530

    quick sort fix

--- src/Main.kt
+++ src/Main.kt
@@ -1,16 +1,17 @@
-import sorting.MergeSort
 import sorting.QuickSort
 
 fun main(){
     val quickSort = QuickSort()
-    val testArray = intArrayOf(12,11,13,1,4,5)
-    quickSort.sort(testArray,0,5)
+    val testArray = intArrayOf(1,41,1,1,1,11)
+    quickSort.threeWayQuickSort(testArray,0,5)
 
 
     testArray.forEach {
         println(it)
     }
 }
+
+
 
 
 
--- src/sorting/QuickSort.kt
+++ src/sorting/QuickSort.kt
@@ -1,27 +1,74 @@
 package sorting
 
-import kotlin.random.Random
 
 class QuickSort {
 
-    fun sort(array: IntArray,l : Int, r: Int){
-        if ( r - l <= 1) return
+    fun sort(array: IntArray, l: Int, r: Int) {
+        if (r - l <= 1) return
         val partitionElement = array[r]
         var m = l
 
-        //intArrayOf(12,11,13,1,4,5) l = 0 , r = 5 , pE = 5
-
-        for(i in l..r){
-            if(array[i] < partitionElement){
-                val temp =array[i]
+        for (i in l..< r) {
+            if (array[i] <= partitionElement) {
+                val temp = array[i]
                 array[i] = array[m]
                 array[m] = temp
                 m++
             }
         }
 
-        sort(array,l,m-1)
-        sort(array,m,r)
+
+        val temp = array[m]
+        array[m] = array[r]
+        array[r] = temp
+
+        sort(array, l, m - 1)
+        sort(array, m+1, r)
 
     }
+
+    fun threeWayQuickSort(array: IntArray,l: Int,r: Int){
+        if(l >= r) return
+        val (lt,gt) = partition(array,l,r)
+
+        threeWayQuickSort(array,l,lt - 1)
+        threeWayQuickSort(array,gt+1,r)
+    }
+
+    fun partition(array: IntArray,l:Int,r:Int):Pair<Int, Int>{
+        val pivot = array[l]
+        var gt = r
+        var lt = l
+        var i = l+1
+
+        while(i <= gt){
+            when {
+                pivot > array[i] -> {
+                    val temp = array[i]
+                    array[i] =  array[lt]
+                    array[lt] = temp
+                    lt++
+                    i++
+                }
+
+                pivot < array[i] -> {
+                    val temp = array[i]
+                    array[i] =  array[gt]
+                    array[gt] = temp
+
+                    gt--
+                   /// i++
+                }
+
+                else -> {
+                    i++
+                }
+            }
+        }
+
+        return Pair(lt,gt)
+
+    }
+
+
 }
